0433beef01ddc48585a4169892d4f71b3d870f59
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2014-12-02 23:54:45 -0500

    fixed solution printing issue

--- src/middleman.cc
+++ src/middleman.cc
@@ -25,7 +25,7 @@ std::vector<node> nodes;
 std::vector<std::vector<edge_endpoint> > graph;
 std::vector<std::vector<middlebox_instance> > deployed_mboxes;
 std::vector<double> deployment_costs, energy_costs, transit_costs, sla_costs,
-total_costs, stretches;
+    total_costs, stretches;
 std::vector<double> net_util;
 double per_core_cost, per_bit_transit_cost;
 double cost[MAXN][MAXN];
@@ -66,7 +66,7 @@ int main(int argc, char *argv[]) {
   if (algorithm == "cplex") {
     std::vector<traffic_request> current_traffic_requests;
     int current_time = traffic_requests[0].arrival_time;
-    double opex, running_time; 
+    double opex, running_time;
     int processed_traffic = 0;
 
     // files to write output
@@ -83,30 +83,33 @@ int main(int argc, char *argv[]) {
       fprintf(cost_log_file, "%d ", current_time);
       fprintf(util_log_file, "%d ", current_time);
 
-      for (;i < traffic_requests.size() && current_time == traffic_requests[i].arrival_time; ++i) {
+      for (; i < traffic_requests.size() &&
+                 current_time == traffic_requests[i].arrival_time;
+           ++i) {
         current_traffic_requests.push_back(traffic_requests[i]);
       }
       current_time = traffic_requests[i].arrival_time;
-      
+
       std::vector<int> sequence[current_traffic_requests.size()];
       int delays[current_traffic_requests.size()];
       std::vector<double> opex_breakdown;
       std::vector<int> utilization;
 
-      run_cplex(current_traffic_requests, opex, opex_breakdown, 
-                running_time, sequence, delays, utilization, topology_filename);
-      
+      run_cplex(current_traffic_requests, opex, opex_breakdown, running_time,
+                sequence, delays, utilization, topology_filename);
+
       processed_traffic += current_traffic_requests.size();
 
-      cout << processed_traffic*100.0/traffic_requests.size() << "% Traffic processed." << endl;
+      cout << processed_traffic * 100.0 / traffic_requests.size()
+           << "% Traffic processed." << endl;
 
-      //cost log
+      // cost log
       for (double cost : opex_breakdown) {
         fprintf(cost_log_file, "%lf ", cost);
       }
       fprintf(cost_log_file, "%lf %lf\n", opex, running_time);
 
-      //sequence log
+      // sequence log
       for (int i = 0; i < current_traffic_requests.size(); ++i) {
         std::vector<int> seq = sequence[i];
         for (int j = 0; j < seq.size(); ++j) {
@@ -115,25 +118,25 @@ int main(int argc, char *argv[]) {
             fprintf(sequence_log_file, ",");
           }
         }
-        //fprintf(sequence_log_file, "%d ", delays[i]);
+        // fprintf(sequence_log_file, "%d ", delays[i]);
         fprintf(sequence_log_file, "\n");
       }
 
-      //utilization log
+      // utilization log
       for (int cores : utilization) {
         fprintf(util_log_file, "%d ", cores);
       }
       fprintf(util_log_file, "\n");
 
       fflush(cost_log_file);
-      fflush(sequence_log_file);  
-      fflush(util_log_file);    
+      fflush(sequence_log_file);
+      fflush(util_log_file);
 
       current_traffic_requests.clear();
-      //exit(0);
+      // exit(0);
     }
 
-    //close all the output files
+    // close all the output files
     fclose(cost_log_file);
     fclose(sequence_log_file);
     fclose(util_log_file);
@@ -144,7 +147,7 @@ int main(int argc, char *argv[]) {
     stats.num_accepted = stats.num_rejected = 0;
     const int kNumTrafficRequests = static_cast<int>(traffic_requests.size());
     for (int i = 0; i < kNumTrafficRequests; ++i) {
-      traffic_requests[i].duration = 6000; //300;
+      traffic_requests[i].duration = 6000; // 300;
       if (current_time != traffic_requests[i].arrival_time) {
         // RefreshServerStats(current_time);
         current_time = traffic_requests[i].arrival_time;
@@ -159,40 +162,6 @@ int main(int argc, char *argv[]) {
       elapsed_time += std::chrono::duration_cast<std::chrono::nanoseconds>(
           solution_end_time - solution_start_time).count();
 
-      // Compute the cost components.
-      /* 
-      double d_cost = 0.0, e_cost = 0.0, t_cost = 0.0, s_cost = 0.0;
-      int prev_node = NIL, current_node = NIL;
-      const int kLastIndex = static_cast<int>(result->size()) - 1;
-      resource resource_vector;
-      for (auto &node : nodes) {
-        resource_vector.cpu_cores.push_back(node.residual_cores);
-      }
-      for (int kk = 1; kk < static_cast<int>(result->size()); ++kk) {
-        auto &m_box =
-            middleboxes[traffic_requests[i].middlebox_sequence[kk - 1]];
-        current_node = result->at(kk);
-        prev_node = result->at(kk - 1);
-        if (kk != kLastIndex)
-          d_cost += GetDeploymentCost(current_node, m_box, traffic_requests[i]);
-        if (kk != kLastIndex)
-          e_cost += GetEnergyCost(current_node, m_box, resource_vector,
-                                  traffic_requests[i]);
-        t_cost += GetTransitCost(prev_node, current_node, traffic_requests[i]);
-        // s_cost +=
-        //    GetSLAViolationCost(prev_node, current_node, traffic_requests[i]);
-        if (kk != kLastIndex &&
-            UsedMiddleboxIndex(current_node, m_box, traffic_requests[i]) ==
-                NIL) {
-          resource_vector.cpu_cores[current_node] -= m_box.cpu_requirement;
-        }
-      }
-      deployment_costs.push_back(d_cost);
-      energy_costs.push_back(e_cost);
-      transit_costs.push_back(t_cost);
-      printf("%.3lf %.3lf\n", e_cost, t_cost);
-      // sla_costs.push_back(s_cost);
-      */
       UpdateResources(result.get(), traffic_requests[i]);
       RefreshServerStats(current_time);
 
@@ -212,28 +181,19 @@ int main(int argc, char *argv[]) {
            100.0 * static_cast<double>(stats.num_accepted) /
                static_cast<double>(stats.num_accepted + stats.num_rejected));
 
-    // Process that collected statistics and write to log.
-    // const std::string kStatsOutputFilePrefix = "log";
-    // ProcessStats(stats, kStatsOutputFilePrefix);
-  }
-
-  // DEBUG: Write all the computed sequences in a file.
-  FILE *all_results_file = fopen("log.sequences", "w");
-  int row_index = 0;
-  for (auto &row : all_results) {
-    for (int i = 0; i < row->size(); ++i) {
-      if (i != 0) fprintf(all_results_file, ",");
-      fprintf(all_results_file, "%d", row->at(i));
+    // DEBUG: Write all the computed sequences in a file.
+    FILE *all_results_file = fopen("log.sequences", "w");
+    int row_index = 0;
+    for (auto &row : all_results) {
+      for (int i = 0; i < row->size(); ++i) {
+        if (i != 0)
+          fprintf(all_results_file, ",");
+        fprintf(all_results_file, "%d", row->at(i));
+      }
+      fprintf(all_results_file, "\n");
+      ++row_index;
     }
-    fprintf(all_results_file, "\n");
-    // double total_cost = deployment_costs[row_index] + energy_costs[row_index] +
-    //                    transit_costs[row_index] + sla_costs[row_index];
-    // fprintf(all_results_file, " %.5lf %.5lf %.5lf %.5lf %.5lf\n",
-    //        deployment_costs[row_index], energy_costs[row_index],
-    //        transit_costs[row_index], sla_costs[row_index], total_cost);
-    ++row_index;
+    fclose(all_results_file);
   }
-  fclose(all_results_file);
-
   return 0;
 }
